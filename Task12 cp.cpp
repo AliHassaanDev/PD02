dfc15f0e470d6ad9e5dc8ccdc69f55d6e61ba269
AliHassaanDev <malihassaan21@gmail.com>
2024-10-21 13:07:43 +0500

    message

---
+++ Task12 cp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("color 07");
+    cout<<"       *     *     *                    "<<endl;
+    cout<<"         *     *     *                  "<<endl;
+    cout<<"          *     *     *                 "<<endl;
+    cout<<"         *     *     *                  "<<endl;
+    cout<<"        *     *     *                   "<<endl;
+    cout<<"         *     *     *                  "<<endl;
+    cout<<"           *     *     *                "<<endl;
+    cout<<"************************************    "<<endl;
+    cout<<" *                                *     "<<endl;
+    cout<<"  *                              *      "<<endl;
+    cout<<"    *                          *        "<<endl;
+    cout<<"      *                      *          "<<endl;
+    cout<<"        ********************            "<<endl;
+    cout<<"                                        "<<endl;
+    cout<<" #WELCOME TO F()():)Y APPLICATION#      "<<endl;
+}
